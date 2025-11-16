9a50e08e8dad862b984f72dafd0d25dbd9058b69
Dittor67 <100069603+Dittor-Aud@users.noreply.github.com>
2025-11-16 15:29:54 +0700

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -50,7 +50,7 @@ export default {
                 </div>
                 <div class="player-container">
                     <div class="player">
-                        <h1>{{ cleanName(ientry.user) }}</h1><p>#{{ selected + 1 }}</p>
+                        <h1>{{ cleanName(entry.user) }}</h1><p>#{{ selected + 1 }}</p>
                         <h3 v-if="entry.total > 0"><b>{{ localize(entry.total) }}</b></h3>
                         <p>Pack Bonus: {{ entry.packBonus }}</p>
                         <div class="packs" v-if="entry.packs.length > 0">
